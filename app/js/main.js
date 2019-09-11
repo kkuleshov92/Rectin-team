ca908b16a22464a74d3160bbbe5e1312057f37ee
Kirill <nstkuleshov1@gmail.com>
2019-09-11 11:37:28 +0300

    preparing works

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,15 @@
 
+//=include ../components/header/header.js
+//=include ../components/reasons/reasons.js
+//=include ../components/ignor/ignor.js
+//=include ../components/func/func.js
+//=include ../components/kind/kind.js
+//=include ../components/usage/usage.js
+//=include ../components/help/help.js
+//=include ../components/search/search.js
+//=include ../components/spec/spec.js
+//=include ../components/reviews/reviews.js
+//=include ../components/order/order.js
+//=include ../components/call/call.js
+//=include ../components/last/last.js
 //=include ../components/footer/footer.js
